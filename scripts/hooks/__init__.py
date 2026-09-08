2ef1a4723ce4708aeab02c58f32f14dffea962ea
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 02:52:04 -0300

    chore: advance all 31 member gitlinks to wave-2 landed tips (flext-pwmej)

--- scripts/hooks/__init__.py
+++ scripts/hooks/__init__.py
@@ -3,9 +3,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from types import MappingProxyType
+from typing import TYPE_CHECKING
 
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
@@ -16,7 +15,7 @@ __all__: tuple[str, ...] = ("c", "d", "e", "h", "m", "p", "r", "s", "t", "u", "x
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            "flext": ("c", "d", "e", "h", "m", "p", "r", "s", "t", "u", "x"),
+            "flext": ("c", "d", "e", "h", "m", "p", "r", "s", "t", "u", "x")
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
