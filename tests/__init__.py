6cca4d206c784fdd09754e84a405b758c6d65b3f
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 12:20:19 -0300

    fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,7 +11,7 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 if TYPE_CHECKING:
     from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
 
-    from . import infra as infra, unit as unit
+    from . import infra, unit
 __all__: tuple[str, ...] = (
     "c",
     "d",
--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -11,7 +11,7 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 if TYPE_CHECKING:
     from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
 
-    from . import libs as libs
+    from . import libs
 __all__: tuple[str, ...] = (
     "c",
     "d",
