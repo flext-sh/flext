846471a25391372c1179cd9072fc93c5a053fca7
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-11 23:29:39 -0300

    chore(projections): absorb regenerated projections

--- tests/infra/__init__.py
+++ tests/infra/__init__.py
@@ -9,15 +9,14 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_tests import FlextTestsConstants, d, e, h, s, td, tf, tk, tm, tv, u, x
+    from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
 
-    from .constants import TestsFlextRootConstants, TestsFlextRootConstants as c
-    from .models import TestsFlextRootModels, TestsFlextRootModels as m
-    from .protocols import TestsFlextRootProtocols, TestsFlextRootProtocols as p
-    from .result import TestsFlextRootResult, r
-    from .typings import TestsFlextRootTypes, TestsFlextRootTypes as t
+    from .constants import TestsFlextRootConstants
+    from .models import TestsFlextRootModels
+    from .protocols import TestsFlextRootProtocols
+    from .result import TestsFlextRootResult
+    from .typings import TestsFlextRootTypes
 __all__: tuple[str, ...] = (
-    "FlextTestsConstants",
     "TestsFlextRootConstants",
     "TestsFlextRootModels",
     "TestsFlextRootProtocols",
@@ -44,17 +43,21 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".constants": ("TestsFlextRootConstants", "c"),
-            ".models": ("TestsFlextRootModels", "m"),
-            ".protocols": ("TestsFlextRootProtocols", "p"),
-            ".result": ("TestsFlextRootResult", "r"),
-            ".typings": ("TestsFlextRootTypes", "t"),
+            ".constants": ("TestsFlextRootConstants",),
+            ".models": ("TestsFlextRootModels",),
+            ".protocols": ("TestsFlextRootProtocols",),
+            ".result": ("TestsFlextRootResult",),
+            ".typings": ("TestsFlextRootTypes",),
             "flext_tests": (
-                "FlextTestsConstants",
+                "c",
                 "d",
                 "e",
                 "h",
+                "m",
+                "p",
+                "r",
                 "s",
+                "t",
                 "td",
                 "tf",
                 "tk",
