3f8a9e9d3d743c54236eba47368de3bc01c9be6d
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 11:41:05 -0300

    chore(gen): root examples projection at the fleet fixed point

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,15 +9,26 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext import c, d, e, h, m, p, r, s, t, u, x
+    from flext import c, m, p, s, t, u
+    from flext_core import d, e, h, r, x
 
     from ._constants import FlextRootExamplesConstants
     from ._models import FlextRootExamplesModels, ValidationRules
 __all__: tuple[str, ...] = (
-    "FlextRootExamplesConstants", "FlextRootExamplesModels", "ValidationRules", "c",
-    "d", "e", "h", "m",
-    "p", "r", "s", "t",
-    "u", "x",
+    "FlextRootExamplesConstants",
+    "FlextRootExamplesModels",
+    "ValidationRules",
+    "c",
+    "d",
+    "e",
+    "h",
+    "m",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "x",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
@@ -25,7 +36,8 @@ _LAZY_IMPORTS = MappingProxyType(
         MappingProxyType({
             "._constants": ("FlextRootExamplesConstants",),
             "._models": ("FlextRootExamplesModels", "ValidationRules"),
-            "flext": ("c", "d", "e", "h", "m", "p", "r", "s", "t", "u", "x"),
+            "flext": ("c", "m", "p", "s", "t", "u"),
+            "flext_core": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
